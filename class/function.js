072ff41dcafb6d5772c22e56b5afca1f41a99acf
Nicholas Morgan <Nicholas.Morgan2002@aol.com>
2023-03-22 10:58:33 -0400

    moved some files around

--- class/function.js
+++ class/function.js
@@ -7,7 +7,43 @@ function getMeow(){
     console.log('meow')
 }
 
-//invoke function
-message('Jarvis')
+function getSum(firstNumber, secondNumber){
+    const result = firstNumber + secondNumber;
+    console.log(result)
+}
 
-getMeow()
+function makePie(quantity){
+    const pie = '🥧'.repeat(quantity);
+    console.log(pie);
+}
+
+function makeAnimal(animal, quantity){
+    const result = animal.repeat(quantity)
+    return result;
+}
+
+function generateLoudSpeaker(text){
+    const nextText = '🔊 ' + text.toUpperCase();
+    return nextText;
+}
+
+//invoke function
+
+// message('Jarvis')
+
+// getMeow()
+
+getSum(2, 5)
+
+// getSum(120000, 120000000)
+
+// getSum('sym', 'phony')
+
+// makePie(100)
+
+// const renderResult = makeAnimal('🐼', 10)
+// console.log(renderResult);
+
+//const computer = generateLoudSpeaker('whisper wh is per')
+//console.log(computer)
+
---
+++ homework/weekOneDayTwo.js
@@ -0,0 +1,34 @@
+const pet = 'sheep'
+
+switch(pet){
+  case 'cat':
+    console.log('meow')
+    break;
+  case 'dog':
+    console.log('woof')
+    break;
+  case 'sheep':
+    console.log('brrr')
+    break;
+  default:
+    console.log('cool pet')
+  }
+
+function total(number, number1){
+    const result = number + number1;
+    console.log(result);
+}
+
+total(2, 2)
+
+let fruit = 'orange'
+
+if(fruit === 'orange'){
+    console.log('orange')
+} else if (fruit === 'apple'){
+    console.log('green')
+} else if (fruit === 'tomato'){
+    console.log('red')
+} else if (fruit === 'bannana'){
+    console.log('yellow')
+}
