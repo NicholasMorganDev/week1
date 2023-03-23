b1535394524004b42aa15faaa609ce895b3d070a
Nicholas Morgan <Nicholas.Morgan2002@aol.com>
2023-03-23 09:57:31 -0400

    organizing some files

---
+++ class/loop-99-bottles.js
@@ -0,0 +1,10 @@
+//for loop for 99 bottles song.
+
+function bottlesOfBeer (){ 
+  for (let i = 99; i>=1; i--){
+    let bottles = i
+    console.log(`${bottles} bottles of beer on the wall. ${bottles} of beer! Take one down pass it around.`)
+  }
+}
+
+bottlesOfBeer()
