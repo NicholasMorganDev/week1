ebeaef86f177170e9a7f1bc9242c2af8650c0923
Nicholas Morgan <Nicholas.Morgan2002@aol.com>
2023-03-25 16:34:01 -0400

    learned that you can use true and false forparameters of a switch statement

--- homework/extra.js
+++ homework/extra.js
@@ -17,8 +17,20 @@ function extra (array) {
   console.log(array[0])
 }
 
-extra(clothing)
+//extra(clothing)
 
 const array = (filler) => {console.log(filler[0])}
 
-array(clothing)
+//array(clothing)
+
+let a = 7
+
+switch (true) {
+  case(a > 6):
+  console.log('asa')
+  break;
+  default:
+  console.log('asasas')
+  break;
+}
+
